9d9cb1cd67fde17d059330dae958565b9a7ec198
David Carlson <mndrake22@mac.com>
2018-09-16 21:48:54 -0500

    updated to python 3.6

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='credit-model',
+    name='credit_model',
     version='0.1dev',
     license='MIT',
     long_description=open('README.txt').read(),
     packages=['credit_model'],
     package_dir={'credit_model': 'credit_model'},
-    package_data = {'credit_model': ['model/nb.gz']}
+    package_data={'credit_model': ['model/nb.gz']}
 )
